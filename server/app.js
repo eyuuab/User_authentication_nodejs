ce1db0e1037e6097ac68009ca22e530883f267e6
eyobed <eyobedabreham9@gmail.com>
2024-08-28 11:36:01 +0300

    add cose

--- server/app.js
+++ server/app.js
@@ -1,12 +1,14 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 
 connectDB();
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api/auth', require('./routes/auth'));
+
 
 const PORT = process.env.PORT || 5000;
 
